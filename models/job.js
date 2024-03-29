2e5fa826ff36e1e695581bcf5841ea8d32eb4dd6
Adrian McBroom <adrianmcb2000@gmail.com>
2024-03-29 09:41:55 -0700

    paused for deployment

---
+++ models/job.js
@@ -0,0 +1,23 @@
+// "use strict";
+
+// const db = require("../db");
+// const { sqlForPartialUpdate } = require("../helpers/sql");
+// const {
+//   NotFoundError,
+//   BadRequestError,
+//   UnauthorizedError,
+// } = require("../expressError");
+
+// const { BCRYPT_WORK_FACTOR } = require("../config.js");
+
+// /** Related functions for users. */
+
+// class Job {
+
+//   static async create() {
+
+//   }
+
+
+
+// }

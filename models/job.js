a1dd2b5e6b4d5f57bed90ab3b588c2fcdc2dcdfb
Adrian McBroom <adrianmcb2000@gmail.com>
2024-03-29 10:36:06 -0700

    created and tested Job.create method

--- models/job.js
+++ models/job.js
@@ -1,23 +1,43 @@
-// "use strict";
+"use strict";
 
-// const db = require("../db");
-// const { sqlForPartialUpdate } = require("../helpers/sql");
-// const {
-//   NotFoundError,
-//   BadRequestError,
-//   UnauthorizedError,
-// } = require("../expressError");
+const db = require("../db");
+const { sqlForPartialUpdate } = require("../helpers/sql");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
 
-// const { BCRYPT_WORK_FACTOR } = require("../config.js");
+const { BCRYPT_WORK_FACTOR } = require("../config.js");
 
-// /** Related functions for users. */
+/** Related functions for users. */
 
-// class Job {
+class Job {
 
-//   static async create() {
+  static async create({ title, salary, equity, companyHandle }) {
 
-//   }
+    const companyCheck = await db.query(`
+        SELECT handle
+        FROM companies
+        WHERE handle = $1`, [companyHandle]);
+
+    if (companyCheck.rows.length === 0)
+      throw new BadRequestError(`Company does not exist: ${companyHandle}`);
+
+    const results = await db.query(`
+      INSERT INTO jobs (title, salary, equity, company_handle)
+        VALUES ($1, $2, $3, $4)
+      RETURNING id, title, salary, equity, company_handle`,
+      [title, salary, equity, companyHandle]);
 
 
 
-// }
+    return results.rows[0];
+  }
+
+
+
+}
+
+
+module.exports = Job;
--- models/job.test.js
+++ models/job.test.js
@@ -1,26 +1,53 @@
-// "use strict";
+"use strict";
 
-// const {
-//   NotFoundError,
-//   BadRequestError,
-//   UnauthorizedError,
-// } = require("../expressError");
-// const db = require("../db.js");
-// const Job = require("./job.js");
-// const {
-//   commonBeforeAll,
-//   commonBeforeEach,
-//   commonAfterEach,
-//   commonAfterAll,
-// } = require("./_testCommon");
+const {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} = require("../expressError");
+const db = require("../db.js");
+const Job = require("./job.js");
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
 
-// beforeAll(commonBeforeAll);
-// beforeEach(commonBeforeEach);
-// afterEach(commonAfterEach);
-// afterAll(commonAfterAll);
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
 
-// describe("Create job", function () {
-//   test("works", function () {
+describe("Create job", function () {
+  test("works", async function () {
+    const results = await Job.create({
+      title: "manager",
+      salary: 100000,
+      equity: 0.8,
+      companyHandle: "c1"
+    });
 
-//   })
-// })
+    expect(results).toEqual({
+      id: expect.any(Number),
+      title: "manager",
+      salary: 100000,
+      equity: "0.8",
+      company_handle: "c1"
+    });
+  });
+
+  test("non-existent company handle", async function () {
+    try {
+      await Job.create({
+        title: "manager",
+        salary: 100000,
+        equity: 0.8,
+        companyHandle: "doesnt_exist"
+      });
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
